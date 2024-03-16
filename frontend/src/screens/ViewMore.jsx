360d01531f20d305aca23d1105b173f2c49329ce
teesha17 <teeshu.2179@gmail.com>
2024-03-16 17:55:37 +0530

    made payment responsive

--- frontend/src/screens/ViewMore.jsx
+++ frontend/src/screens/ViewMore.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-export default function ViewMore() {
-    const data=JSON.parse(localStorage.getItem("cardData"));
-  return (
-    <div style={{"display":"flex"}}>
-        
-        <img src={data.img} style={{"height":"500px","margin":"60px"}}/>
-       <div style={{"textAlign":"center"}}><h1 style={{"margin":"0px 60px"}}>{data.name}</h1>
-       <p className='m-3'>Rs. {data.price}</p>
-        <button className='btn btn-success rounded m-3' ><Link to="https://wa.me/qr/4LS4RESPI4LFI1" style={{"textDecoration":"none" ,"color":"white"}}>Chat On WhatsApp</Link></button></div>
 
-    </div>
-  )
+export default function ViewMore() {
+    const data = JSON.parse(localStorage.getItem("cardData"));
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={data.img} className="img-fluid" alt={data.name} />
+                </div>
+                <div className="col-md-6 d-flex align-items-center">
+                    <div>
+                        <h1>{data.name}</h1>
+                        <p className="m-3">Rs. {data.price}</p>
+                        <button className="btn btn-success rounded m-3">
+                            <a href="https://wa.me/qr/4LS4RESPI4LFI1" target="_blank" rel="noopener noreferrer" style={{ "textDecoration": "none", "color": "white" }}>Chat On WhatsApp</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
